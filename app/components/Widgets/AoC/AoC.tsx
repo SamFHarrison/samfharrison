f3b2d48346e26073bbc4fe2ce1fca6a70354f62a
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-12-13 00:58:49 +0000

    fix(widgets): swap slash for the html entities

--- app/components/Widgets/AoC/AoC.tsx
+++ app/components/Widgets/AoC/AoC.tsx
@@ -7,7 +7,7 @@ const AoC = () => {
     <Widget size="small" className="aoc" href={LINKS.GitHub.AoC}>
       <h2 className="aoc-body">Advent of Code</h2>
       <p className="aoc-body">
-        <span className="aoc-body-secondary">//</span> 2024
+        <span className="aoc-body-secondary">&#47;&#47;</span> 2024
       </p>
     </Widget>
   );
