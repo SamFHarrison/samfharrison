652d19328b1bb6a2efd4180997e6b790fd1528f6
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-04-05 08:32:12 +0100

    feat: build hero section + move old components to archive directory

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,18 @@ import "./page.css";
 export default function Home() {
   return (
     <main>
-      <Image
-        src={Avatar}
-        alt="Digital avatar of Sam, he has blonde hair, glasses and a friendly expression, styled in a cartoonish emoji-like appearance"
-        className="hero-widget-avatar"
-        width={100}
-        height={100}
-        priority
-      />
-
-      <h1>Hi, I&apos;m Sam</h1>
+      <div className="hero-container">
+        <Image
+          src={Avatar}
+          alt="Digital avatar of Sam, he has blonde hair, glasses and a friendly expression, styled in a cartoonish emoji-like appearance"
+          className="hero-widget-avatar"
+          width={75}
+          height={75}
+          priority
+        />
+        <h1>Sam F-Harrison</h1>
+        <p>Designing and building applications for the web and mobile</p>
+      </div>
     </main>
   );
 }
