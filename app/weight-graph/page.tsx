87b33283895c3fc90b830af5eacc8585d4ee03c5
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-04-06 10:30:03 +0100

    feat: [wip] add first body weight graph

---
+++ app/weight-graph/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import WeightGraph from "../components/WeightGraph/WeightGraph";
+
+const page = () => {
+  return <WeightGraph />;
+};
+
+export default page;
