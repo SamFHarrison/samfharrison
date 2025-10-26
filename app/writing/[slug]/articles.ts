dc97703327cafa311074bb3573eb81d1351373f3
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-10-26 20:43:24 +0000

    feat(writing): add dynamic routing for articles
    add directory to keep all articles in + add dynamic routing based on a list of stored articles

---
+++ app/writing/[slug]/articles.ts
@@ -0,0 +1,13 @@
+export interface Article {
+  title: string;
+  slug: string;
+}
+
+type ArticleList = Article[];
+
+export const articleList: ArticleList = [
+  {
+    title: "Lego F1 Wall Art",
+    slug: "lego-f1-wall-art",
+  },
+];
---
+++ app/writing/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { articleList } from './articles';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const { default: Article } = await import(`../articles/${slug}.mdx`);
+
+  return <Article />;
+}
+
+export function generateStaticParams() {
+  const articleSlugs = articleList.map((article) => {
+    return {
+      slug: article.slug,
+    };
+  });
+
+  return articleSlugs;
+}
+
+export const dynamicParams = false;
