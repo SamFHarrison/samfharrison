6cc5b7a7e2674b76361c54f5846d446e2696d90b
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-10-28 23:08:41 +0000

    feat(mdx): add support for secondary text in mdx
    map p element with the secondary classname to mdx component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,14 @@
 import type { MDXComponents } from "mdx/types";
 
-const components: MDXComponents = {};
+const components: MDXComponents = {
+  SecondaryText({ children, props }) {
+    return (
+      <p className="secondary-text" {...props}>
+        {children}
+      </p>
+    );
+  },
+};
 
 export function useMDXComponents(): MDXComponents {
   return components;
