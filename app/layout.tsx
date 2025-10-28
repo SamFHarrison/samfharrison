13fb05414e54885d286654c309858d1acacf1f24
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-10-28 23:07:21 +0000

    feat: typography setup
    set up css styling for h1, h2, p, hr and secondary text + add central layout to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 import Script from 'next/script';
 import { BIO, KEYWORDS, PERSON_SCHEMA } from './seo';
 import './globals.css';
-
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist",
-});
-
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-});
 
 export const generateMetadata = (): Metadata => {
   return {
@@ -31,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geist.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${GeistSans.className} ${GeistMono.variable}`}>
       <body>
         {children}
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <main role="main">
-      <h1>Sam F-Harrison</h1>
-    </main>
+    <>
+      <header>
+        <p className="secondary-text">samfharrison.com</p>
+        <h1>Sam F-Harrison</h1>
+        <p className="secondary-text">Software Engineer, UI</p>
+        <hr />
+      </header>
+
+      <main>
+        <p>
+          This is the corner of the web where I keep some code, some thoughts
+          and some memories I want to keep.
+        </p>
+
+        <section>
+          <h2>/Writing</h2>
+          <p>
+            I think I'll use this as a digital scrapbook - including TIL's,
+            note-to-self's and maybe even the odd opinion or two.
+          </p>
+          <p className="secondary-text">coming soon...</p>
+        </section>
+      </main>
+      <footer>
+        <hr />
+        <div aria-label="Social links"></div>
+      </footer>
+    </>
   );
 }
