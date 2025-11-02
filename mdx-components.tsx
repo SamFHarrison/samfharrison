704a9c5dd7dd2e93d386cc7edb49cf3a1bd2039e
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-11-02 21:27:20 +0000

    feat(mdx): add syntax highlighting for mdx code blocks + one dark theme

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,5 @@
 import type { MDXComponents } from "mdx/types";
+import CodeBlock from "./app/components/CodeBlock/CodeBlock";
 
 const components: MDXComponents = {
   SecondaryText({ children, props }) {
@@ -7,6 +8,21 @@ const components: MDXComponents = {
         {children}
       </p>
     );
+  },
+
+  pre: ({ children, ...props }: any) => {
+    const preProps = children?.props;
+
+    const raw =
+      typeof preProps?.children === "string"
+        ? preProps.children
+        : Array.isArray(preProps?.children)
+        ? preProps.children.join("")
+        : "";
+
+    const lang = (preProps?.className || "").replace("language-", "") || "ts";
+
+    return <CodeBlock code={raw} lang={lang as any} {...props} />;
   },
 };
 
