7f765909ae2ee61eca9057bcfc9f52d155b162f2
Sam F-Harrison <sgfletcherharrison@gmail.com>
2025-11-06 17:36:28 +0000

    feat(writing): add article
    title: 'Feel like shit' Checklist

--- app/writing/[slug]/articles.ts
+++ app/writing/[slug]/articles.ts
@@ -10,4 +10,8 @@ export const articleList: ArticleList = [
     title: "How I Git",
     slug: "how-i-git",
   },
+  {
+    title: "'Feel like shit' Checklist",
+    slug: "feel-like-shit-checklist",
+  },
 ];
