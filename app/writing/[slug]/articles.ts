d4d8963e8616e08849b8ae28030280d6d646c15a
Sam F-Harrison <sgfletcherharrison@gmail.com>
2026-01-22 23:18:42 +0000

    feat(articles): add 2025 review

--- app/writing/[slug]/articles.ts
+++ app/writing/[slug]/articles.ts
@@ -7,6 +7,10 @@ type ArticleList = Article[];
 
 export const articleList: ArticleList = [
   {
+    title: "A Deep Reflection of 2025",
+    slug: "a-deep-reflection-of-2025",
+  },
+  {
     title: "How I Git",
     slug: "how-i-git",
   },
