2bf740146283cf81884e01d8f069677d5f7077d0
Sam F-Harrison <sgfletcherharrison@gmail.com>
2026-08-24 04:19:02 +0100

    add /notes route ready to deprecate /writing route after removing mdx engine

---
+++ app/notes/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { articleList } from "../writing/[slug]/articles";
+import { ROUTES } from "../lib/contants";
+import NavLink from "../lib/components/HomeLink/NavLink";
+
+export default function Notes() {
+  return (
+    <main>
+      <nav>
+        <NavLink href={ROUTES.Home} label="Home" />
+      </nav>
+
+      <h1>Notes</h1>
+
+      <hr />
+
+      <ul>
+        {articleList.map((article) => {
+          return (
+            <li key={article.slug}>
+              <Link href={`${ROUTES.Writing}/${article.slug}`}>
+                {article.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+}
