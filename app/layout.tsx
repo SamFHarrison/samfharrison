e37d003246680626e67c95c1da09f2f6b617ac4f
Sam F-Harrison <sgfletcherharrison@gmail.com>
2026-08-24 04:18:10 +0100

    replace home link with specific nav link component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Footer } from "./lib/components";
-import HomeLink from "./lib/components/HomeLink/HomeLink";
 import "./colours.css";
 import "./globals.css";
 
@@ -27,8 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.className} ${GeistMono.variable}`}>
       <body>
-        <HomeLink />
-
         {children}
 
         <Footer />
